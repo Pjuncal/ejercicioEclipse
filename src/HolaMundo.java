643a6e643238998b8b8a0158073edf62c37def6b
Patri Juncal <pjuncal@viewnext.com>
2018-03-08 17:15:19 +0100

    EjercicioEclipse 4

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,9 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo Que tal? Me has escuchado. Muy bien. ");
+		System.out.println("Hola Mundo Que tal?");
+		System.out.println("Me has escuchado.");
+		System.out.println("Muy bien. ");
 	}
 
 }
